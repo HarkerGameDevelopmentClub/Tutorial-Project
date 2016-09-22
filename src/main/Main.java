bda4598cd44ef6fb5e76888d6e6e150f323bdf78
18praveenb <Praveen@Shire.local>
2016-09-22 15:23:22 -0700

    Initial Commit

---
+++ src/main/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Packages are like namespaces.
+ */
+package main;
+
+/**
+ * This class will teach you how to integrate JavaFX into a project.
+ * Note that if you create a new JavaFX project you need to make the following
+ * edit to make it compile properly in Eclipse:
+ * 1. Right click the project folder
+ * 2. Select "Properties"
+ * 3. Select "Java Build Path" from the properties window sidebar
+ * 4. Select "Libraries" and expand the disclosure triangle for "JRE System Library"
+ * 5. Double click "Access rules" and "Add" a new rule with "Resolution: Accessible"
+ * and rule pattern "javafx/**" (no quotes when you enter it).
+ * @author Praveen
+ *
+ */
+public class Main {
+
+}
